f6cf8cde61a289fcca504086480236b999c3faf5
chanawee180303 <chanawee.k@kkumail.com>
2023-08-31 16:54:46 +0700

    first commit

---
+++ app/Http/Controllers/UserroleController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserroleController extends Controller
+{
+    public function index(){
+            $role = [
+                ['type' => 'อาจารย์', 'name' => ['อาจารย์จักรกฤษณ์']],
+                ['type' => 'ทีเอ', 'name' => ['ทีเอพี่โอม', 'ทีเอพี่เฟรนด์']],
+                ['type' => 'นักเรียน', 'name' => ['สมศี มะนอง', 'พิโธ่ ชินวัด']]
+            ];
+    }
+}
